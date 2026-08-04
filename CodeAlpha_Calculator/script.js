afb176275c1b5ee58836f379b66bc69e8f430a67
Adewole Oluwasegun <adewoleoluwasegun7@gmail.com>
2026-08-04 09:38:09 +0100

    final value reset to initial state

--- CodeAlpha_Calculator/script.js
+++ CodeAlpha_Calculator/script.js
@@ -1,15 +1,20 @@
+const calculationDisplay = document.querySelector('.calculation-display');
+console.log(calculationDisplay);
 let finalValue = ''
 const addToFinalValue = (value) =>{
     finalValue = finalValue + value
     console.log(finalValue);
+    calculationDisplay.innerHTML = finalValue;
 }
 const convertToCalculation = () =>{
  finalValue= eval(finalValue);
  console.log(finalValue);
- finalValue = '';
+ calculationDisplay.innerHTML = finalValue;
+ finalValue =''
 }
 const backspaceFinalValue = () =>{
     let newStr = finalValue.slice(0, -1);
     finalValue = newStr;
+    calculationDisplay.innerHTML = finalValue;
     console.log(finalValue);
 }
