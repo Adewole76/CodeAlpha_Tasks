4d5aac120fd3181b542eef8b6a2f7054ba2c2e9b
Adewole Oluwasegun <adewoleoluwasegun7@gmail.com>
2026-08-06 15:52:06 +0100

    filtering functionality added

--- CodeAlpha_Image-gallery/script.js
+++ CodeAlpha_Image-gallery/script.js
@@ -1,112 +1,148 @@
-const Images = [
+const imagesContainer = document.querySelector('.images-container');
+const filterButtons = document.querySelectorAll('.filter')
+console.log(filterButtons)
+console.log(imagesContainer)
+let Images = [
     {
-        ImageSource: '/images/Stuff.jpg',
+        ImageSource: 'images/Stuff.jpg',
         ImageCategory: 'Nature',
-        ImageCaption: '',
+        ImageCaption: 'God of war',
         ImageId: 1
     },
 
     {
-        ImageSource: '/images/Stuff2.jpg',
+        ImageSource: 'images/Stuff2.jpg',
         ImageCategory: 'Portrait',
-        ImageCaption: '',
-        ImageId: 1
+        ImageCaption: 'Living Room',
+        ImageId: 2
     },
     {
-        ImageSource: '/images/Stuff3.jpg',
+        ImageSource: 'images/Stuff3.jpg',
         ImageCategory: 'Travel',
-        ImageCaption: '',
-        ImageId: 1
+        ImageCaption: 'Architecture',
+        ImageId: 3
     },
     {
-        ImageSource: '/images/Stuff4.jpg',
+        ImageSource: 'images/Stuff4.jpg',
         ImageCategory: 'Nature',
-        ImageCaption: '',
-        ImageId: 1
+        ImageCaption: 'Play room',
+        ImageId: 4
     },
     {
-        ImageSource: '/images/Stuff5.jpg',
+        ImageSource: 'images/Stuff5.jpg',
         ImageCategory: 'Portrait',
-        ImageCaption: '',
-        ImageId: 1
+        ImageCaption: 'Street',
+        ImageId: 5
     },
     {
-        ImageSource: '/images/Stuff6.jpg',
+        ImageSource: 'images/Stuff6.jpg',
         ImageCategory: 'Travel',
-        ImageCaption: '',
-        ImageId: 1
+        ImageCaption: 'bus',
+        ImageId: 6
     },
     {
-        ImageSource: '/images/Stuff7.jpg',
+        ImageSource: 'images/Stuff7.jpg',
         ImageCategory: 'Nature',
-        ImageCaption: '',
-        ImageId: 1
+        ImageCaption: 'Restaurant',
+        ImageId: 7
     },
     {
-        ImageSource: '/images/Stuff8.jpg',
+        ImageSource: 'images/Stuff8.jpg',
         ImageCategory: 'Portrait',
-        ImageCaption: '',
-        ImageId: 1
+        ImageCaption: 'Art',
+        ImageId: 8
     },
     {
-        ImageSource: '/images/Stuff9.jpg',
-        ImageCategory: '',
-        ImageCaption: '',
-        ImageId: 1
+        ImageSource: 'images/Stuff9.jpg',
+        ImageCategory:'Nature',
+        ImageCaption: 'Chinese restaurant',
+        ImageId: 9
     },
     {
-        ImageSource: '/images/Stuff10.jpg',
+        ImageSource: 'images/Stuff10.jpg',
         ImageCategory: 'Travel',
-        ImageCaption: '',
-        ImageId: 1
+        ImageCaption: 'Road Street',
+        ImageId: 10
     },
     {
-        ImageSource: '/images/Stuff11.jpg',
+        ImageSource: 'images/Stuff11.jpg',
         ImageCategory: 'Nature',
-        ImageCaption: '',
-        ImageId: 1
+        ImageCaption: 'City View',
+        ImageId: 11
     },
     {
-        ImageSource: '/images/Stuff12.jpg',
-        ImageCategory: '',
-        ImageCaption: '',
-        ImageId: 1
-    },
-    {
-        ImageSource: '/images/Stuff13.jpg',
+        ImageSource: 'images/Stuff12.jpg',
         ImageCategory: 'Portrait',
-        ImageCaption: '',
-        ImageId: 1
+        ImageCaption: 'Wall Art',
+        ImageId: 12
     },
     {
-        ImageSource: '/images/Stuff14.jpg',
-        ImageCategory: 'Travel',
-        ImageCaption: '',
-        ImageId: 1
-    },
-    {
-        ImageSource: '/images/Stuff15.jpg',
-        ImageCategory: 'Nature',
-        ImageCaption: '',
-        ImageId: 1
-    },
-    {
-        ImageSource: '/images/Stuff16.jpg',
+        ImageSource: 'images/Stuff13.jpg',
         ImageCategory: 'Portrait',
-        ImageCaption: '',
-        ImageId: 1
+        ImageCaption: 'Old TV',
+        ImageId: 13
     },
     {
-        ImageSource: '/images/Stuff17.jpg',
+        ImageSource: 'images/Stuff14.jpg',
         ImageCategory: 'Travel',
-        ImageCaption: '',
-        ImageId: 1
+        ImageCaption: 'Shadow',
+        ImageId: 14
     },
     {
-        ImageSource: '/images/Stuff18.jpg',
+        ImageSource: 'images/Stuff15.jpg',
         ImageCategory: 'Nature',
-        ImageCaption: '',
-        ImageId: 1
+        ImageCaption: 'Girl Working on Compute',
+        ImageId: 15
+    },
+    {
+        ImageSource: 'images/Stuff16.jpg',
+        ImageCategory: 'Portrait',
+        ImageCaption: 'Wall SHdow',
+        ImageId: 16
+    },
+    {
+        ImageSource: 'images/Stuff17.jpg',
+        ImageCategory: 'Travel',
+        ImageCaption: 'Detroit',
+        ImageId: 17
+    },
+    {
+        ImageSource: 'images/Stuff18.jpg',
+        ImageCategory: 'Nature',
+        ImageCaption: 'Clothes Store',
+        ImageId: 18
     },
 
 ]
+const mappingImagesArray = (arr) => {
+const mappedImagesArray = arr.map((image,index) => {
+    return `
+    <div class="image-container">
+    <img class="actual-image" src="${image.ImageSource}">
+    <div class="image-overlay"></div>
+    <p class="image-caption">${image.ImageCaption}</p>
+    </div>
+    `
+}).join('')
+imagesContainer.innerHTML = mappedImagesArray;
+}
+mappingImagesArray(Images);
+
+const filteringImagesArray = (buttonString) => {
+    const filteredImagesArray = Images.filter(image => image.ImageCategory === buttonString)
+   console.log(filteredImagesArray.length)
+   mappingImagesArray(filteredImagesArray);
+}
+
+
+for(let i = 0; i < filterButtons.length; i++){
+    if(filterButtons[i].textContent === 'All'){
+        filterButtons[i].addEventListener('click',function(){
+        mappingImagesArray(Images);
+        })
+    }else{
+    filterButtons[i].addEventListener('click',function(){
+        filteringImagesArray(filterButtons[i].textContent)
+    })
+}
+}
